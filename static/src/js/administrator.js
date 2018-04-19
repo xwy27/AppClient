c02d47837c7d66a2ceea3d332e39e520c29add38
Hiyoung.Tsui <992896924@qq.com>
2018-04-19 17:40:54 +0800

    add admin

---
+++ static/src/js/administrator.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        activeTab: 'ImageList',
+        images: [
+            {name:'A1 image', status:'未分配'},
+            {name:'B1 image', status:'未分配'},
+            {name:'C1 image', status:'未分配'},
+            {name:'D1 image', status:'进行中'},
+            {name:'E1 image', status:'进行中'},
+            {name:'F1 image', status:'有分歧'},
+            {name:'G1 image', status:'有分歧'},
+            {name:'H1 image', status:'已完成'},
+        ]
+    },
+    methods: {
+        clickTab: function(tab, event) {
+            activeTab = tab.$el.innerText;
+            console.log(activeTab);
+        }
+    }
+})
--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,12 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-        activeTab: 'login',
         isLogin: true
     },
     methods: {
         clickTab: function() {
             this.isLogin = !this.isLogin;
+        },
+        login: function() {
+            window.location.href = "./administrator/index.html";
         }
     }
 })
