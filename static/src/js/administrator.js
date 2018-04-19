11b646ffe7c9f01fab6c2e24485c26c0f793e109
Hiyoung.Tsui <992896924@qq.com>
2018-04-19 20:00:34 +0800

    update

--- static/src/js/administrator.js
+++ static/src/js/administrator.js
@@ -10,16 +10,50 @@ var app = new Vue({
             {name:'E1 image', status:'进行中'},
             {name:'F1 image', status:'有分歧'},
             {name:'G1 image', status:'有分歧'},
-            {name:'H1 image', status:'已完成'},
-        ]
+            {name:'H1 image', status:'已完成'}
+        ],
+        tasks: [
+            {name:'A1 task', status:'进行中'},
+            {name:'B1 task', status:'进行中'},
+            {name:'C1 task', status:'进行中'},
+            {name:'D1 task', status:'已提交'}
+        ],
+        accounts: [
+            {id:'A1', status:'无权限', email:'aaa@bb.com'},
+            {id:'B2', status:'无权限', email:'aaa@bb.com'},
+            {id:'C3', status:'医生', email:'aaa@bb.com'},
+            {id:'D4', status:'医生', email:'aaa@bb.com'},
+            {id:'E5', status:'客人', email:'aaa@bb.com'},
+            {id:'F6', status:'客人', email:'aaa@bb.com'},
+            {id:'G7', status:'客人', email:'aaa@bb.com'},
+            {id:'H8', status:'客人', email:'aaa@bb.com'},
+        ],
+        breadcrumb: ['图像列表'],
+        currentPage: 'ImageList'
     },
     methods: {
         clickTab: function(tab, event) {
-            activeTab = tab.$el.innerText;
-            console.log(activeTab);
+            // console.log(tab.$el.id.split('-')[1]);
+            if (tab.$el.innerText) {
+                this.breadcrumb = [tab.$el.innerText];
+            } else {
+                this.breadcrumb = [this.breadcrumb[0]];
+            }
+            this.currentPage = tab.$el.id.split('-')[1];
         },
         goHome: function() {
             window.location.href = "../index.html";
+        },
+        clickImage: function(row, event, column) {
+            this.breadcrumb.push('图像详情');
+            this.currentPage = 'ImageInfo';
+        },
+        clickTask: function(row, event, column) {
+            this.breadcrumb.push('任务详情');
+            this.currentPage = 'TaskInfo';
+        },
+        clickAccount: function(row, event, column) {
+            // ...
         }
     }
 })
