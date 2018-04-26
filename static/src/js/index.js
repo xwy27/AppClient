4051250831f120c0632d38adf71f908050427081
Hiyoung.Tsui <992896924@qq.com>
2018-04-26 19:48:58 +0800

    add register func & upload photo

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -6,7 +6,15 @@ var app = new Vue({
             account: '',
             password: ''
         },
-        registerForm: {}
+        hasPhoto: false,
+        photoUrl: '',
+        registerForm: {
+            id: '',
+            account: '',
+            email: '',
+            password: '',
+            photo: ''
+        }
     },
     methods: {
         clickTab: function() {
@@ -16,7 +24,7 @@ var app = new Vue({
             window.location.href = "./administrator/index.html";
             // console.log(this.loginForm);
             // var request = $.ajax({
-            //     url: "http://172.19.103.164:5000/authorization",
+            //     url: "",
             //     method: "POST",
             //     data: {
             //         "username": this.loginForm.account,
@@ -25,13 +33,53 @@ var app = new Vue({
             //     dataType: "application/json"
             // });
             
-            // request.done(function( msg ) {
-            //     console.log(msg);
+            // request.done(function(res) {
+            //     console.log(res);
             // });
             
-            // request.fail(function( jqXHR, textStatus ) {
-            //     alert( "Request failed: " + textStatus );
+            // request.fail(function(xhr, textStatus) {
+            //     alert("Request failed: " + textStatus);
             // });
+        },
+        register: function() {
+            // console.log(this.registerForm);
+            // var request = $.ajax({
+            //     url: "",
+            //     method: "POST",
+            //     data: {
+            //         // ...
+            //     },
+            //     dataType: "application/json"
+            // });
+            
+            // request.done(function(res) {
+            //     console.log(res);
+            //     this.$refs.upload.submit();
+            // });
+            
+            // request.fail(function(xhr, textStatus) {
+            //     alert("Request failed: " + textStatus);
+            // });
+        },
+        uploadPhoto: function(file) {
+            console.log(file);
+            const isJPGorPNG = (file.raw.type === 'image/jpeg' || file.raw.type === 'image/png');
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPGorPNG) {
+                this.$message.error('上传头像图片只能是 JPG 或 PNG 格式!');
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            if (isJPGorPNG && isLt2M) {
+                this.registerForm.photo = file.name;
+                console.log(this.registerForm.photo);
+                this.photoUrl = URL.createObjectURL(file.raw);
+                this.hasPhoto = true;
+            }
+
+            return isJPGorPNG && isLt2M;
         }
     }
 })
